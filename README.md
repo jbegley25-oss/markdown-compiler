922b400dd56748ed35919dedec6f925534f9c698
Jack <jbegley25-oss@users.noreply.github.com>
2026-03-07 16:46:06 -0800

    Final Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Markdown to HTML compiler
+
+![](https://github.com/jbegley25-oss/markdown-compiler/workflows/doctests/badge.svg)&nbsp;
+![](https://github.com/jbegley25-oss/markdown-compiler/workflows/flake8/badge.svg)&nbsp;
+![](https://github.com/jbegley25-oss/markdown-compiler/workflows/command_line/badge.svg)&nbsp;
+
+A simple project for converting markdown files to HTML.
+
+Basic usage:
+```
+$ markdown-compiler example/README.md
+```
+
+<img src='examples/example.png' width=300px>
+
+Fancy CSS formatting can be included with the flag `--add_css`:
+```
+$ markdown-compiler example/README.md --add_css
+```
+
+<img src='examples/example-css.png' width=300px>
---
+++ markdown_compiler/util/line_functions.py
@@ -0,0 +1,217 @@
+'''
+Each of the functions in this file takes a single line of input and transforms the line in some way.
+'''
+
+import re
+
+
+def compile_headers(line):
+    '''
+    Convert markdown headers into <h1>,<h2>,etc tags.
+
+    HINT:
+    This is the simplest function to implement in this assignment.
+    Use a slices to extract the first part of the line,
+    then use if statements to check if they match the appropriate header markdown commands.
+
+    >>> compile_headers('# This is the main header')
+    '<h1> This is the main header</h1>'
+    >>> compile_headers('## This is a sub-header')
+    '<h2> This is a sub-header</h2>'
+    >>> compile_headers('### This is a sub-header')
+    '<h3> This is a sub-header</h3>'
+    >>> compile_headers('#### This is a sub-header')
+    '<h4> This is a sub-header</h4>'
+    >>> compile_headers('##### This is a sub-header')
+    '<h5> This is a sub-header</h5>'
+    >>> compile_headers('###### This is a sub-header')
+    '<h6> This is a sub-header</h6>'
+    >>> compile_headers('      # this is not a header')
+    '      # this is not a header'
+    '''
+    for i in range(6, 0, -1):
+        if line.startswith('#' * i + ' '):
+            return f'<h{i}>{line[i:]}</h{i}>'
+    return line
+
+
+def compile_italic_star(line):
+    '''
+    Convert "*italic*" into "<i>italic</i>".
+
+    HINT:
+    Italics require carefully tracking the beginning and ending positions of the text to be replaced.
+    This is similar to the `delete_HTML` function that we implemented in class.
+    It's a tiny bit more complicated since we are not just deleting substrings from the text,
+    but also adding replacement substrings.
+
+    >>> compile_italic_star('*This is italic!* This is not italic.')
+    '<i>This is italic!</i> This is not italic.'
+    >>> compile_italic_star('*This is italic!*')
+    '<i>This is italic!</i>'
+    >>> compile_italic_star('This is *italic*!')
+    'This is <i>italic</i>!'
+    >>> compile_italic_star('This is not *italic!')
+    'This is not *italic!'
+    >>> compile_italic_star('*')
+    '*'
+    '''
+    return re.sub(r'(?<!\*)\*(?!\*)(.+?)(?<!\*)\*(?!\*)', r'<i>\1</i>', line)
+
+
+def compile_italic_underscore(line):
+    '''
+    Convert "_italic_" into "<i>italic</i>".
+
+    HINT:
+    This function is almost exactly the same as `compile_italic_star`.
+
+    >>> compile_italic_underscore('_This is italic!_ This is not italic.')
+    '<i>This is italic!</i> This is not italic.'
+    >>> compile_italic_underscore('_This is italic!_')
+    '<i>This is italic!</i>'
+    >>> compile_italic_underscore('This is _italic_!')
+    'This is <i>italic</i>!'
+    >>> compile_italic_underscore('This is not _italic!')
+    'This is not _italic!'
+    >>> compile_italic_underscore('_')
+    '_'
+    '''
+    return re.sub(r'(?<!_)_(?!_)(.+?)(?<!_)_(?!_)', r'<i>\1</i>', line)
+
+
+def compile_strikethrough(line):
+    '''
+    Convert "~~strikethrough~~" to "<ins>strikethrough</ins>".
+
+    HINT:
+    The strikethrough annotations are very similar to implement as the italic function.
+    The difference is that there are two delimiting characters instead of one.
+    This will require carefully thinking about the range of your for loop and all of your list indexing.
+
+    >>> compile_strikethrough('~~This is strikethrough!~~ This is not strikethrough.')
+    '<ins>This is strikethrough!</ins> This is not strikethrough.'
+    >>> compile_strikethrough('~~This is strikethrough!~~')
+    '<ins>This is strikethrough!</ins>'
+    >>> compile_strikethrough('This is ~~strikethrough~~!')
+    'This is <ins>strikethrough</ins>!'
+    >>> compile_strikethrough('This is not ~~strikethrough!')
+    'This is not ~~strikethrough!'
+    >>> compile_strikethrough('~~')
+    '~~'
+    '''
+    return re.sub(r'~~(.+?)~~', r'<ins>\1</ins>', line)
+
+
+def compile_bold_stars(line):
+    '''
+    Convert "**bold**" to "<b>bold</b>".
+
+    HINT:
+    This function is similar to the strikethrough function.
+
+    >>> compile_bold_stars('**This is bold!** This is not bold.')
+    '<b>This is bold!</b> This is not bold.'
+    >>> compile_bold_stars('**This is bold!**')
+    '<b>This is bold!</b>'
+    >>> compile_bold_stars('This is **bold**!')
+    'This is <b>bold</b>!'
+    >>> compile_bold_stars('This is not **bold!')
+    'This is not **bold!'
+    >>> compile_bold_stars('**')
+    '**'
+    '''
+    return re.sub(r'\*\*(.+?)\*\*', r'<b>\1</b>', line)
+
+
+def compile_bold_underscore(line):
+    '''
+    Convert "__bold__" to "<b>bold</b>".
+
+    HINT:
+    This function is similar to the strikethrough function.
+
+    >>> compile_bold_underscore('__This is bold!__ This is not bold.')
+    '<b>This is bold!</b> This is not bold.'
+    >>> compile_bold_underscore('__This is bold!__')
+    '<b>This is bold!</b>'
+    >>> compile_bold_underscore('This is __bold__!')
+    'This is <b>bold</b>!'
+    >>> compile_bold_underscore('This is not __bold!')
+    'This is not __bold!'
+    >>> compile_bold_underscore('__')
+    '__'
+    '''
+    return re.sub(r'__(.+?)__', r'<b>\1</b>', line)
+
+
+def compile_code_inline(line):
+    '''
+    Add <code> tags.
+
+    HINT:
+    This function is like the italics functions because inline code uses only a single character as a delimiter.
+    It is more complex, however, because inline code blocks can contain valid HTML inside of them,
+    but we do not want that HTML to get rendered as HTML.
+    Therefore, we must convert the `<` and `>` signs into `&lt;` and `&gt;` respectively.
+
+    >>> compile_code_inline('You can use backticks like this (`1+2`) to include code in the middle of text.')
+    'You can use backticks like this (<code>1+2</code>) to include code in the middle of text.'
+    >>> compile_code_inline('This is inline code: `1+2`')
+    'This is inline code: <code>1+2</code>'
+    >>> compile_code_inline('`1+2`')
+    '<code>1+2</code>'
+    >>> compile_code_inline('This example has html within the code: `<b>bold!</b>`')
+    'This example has html within the code: <code>&lt;b&gt;bold!&lt;/b&gt;</code>'
+    >>> compile_code_inline('this example has a math formula in the  code: `1 + 2 < 4`')
+    'this example has a math formula in the  code: <code>1 + 2 &lt; 4</code>'
+    >>> compile_code_inline('this example has a <b>math formula</b> in the  code: `1 + 2 < 4`')
+    'this example has a <b>math formula</b> in the  code: <code>1 + 2 &lt; 4</code>'
+    >>> compile_code_inline('```')
+    '```'
+    >>> compile_code_inline('```python3')
+    '```python3'
+    '''
+    def replace(m):
+        code = m.group(1).replace('<', '&lt;').replace('>', '&gt;')
+        return '<code>' + code + '</code>'
+    return re.sub(r'(?<!`)`(?!`)(.+?)(?<!`)`(?!`)', replace, line)
+
+
+def compile_links(line):
+    '''
+    Add <a> tags.
+
+    HINT:
+    The links and images are potentially more complicated because they have many types of delimeters: `[]()`.
+    These delimiters are not symmetric, however, so we can more easily find the start and stop locations using the strings find function.
+
+    >>> compile_links('Click on the [course webpage](https://github.com/mikeizbicki/cmc-csci040)!')
+    'Click on the <a href="https://github.com/mikeizbicki/cmc-csci040">course webpage</a>!'
+    >>> compile_links('[course webpage](https://github.com/mikeizbicki/cmc-csci040)')
+    '<a href="https://github.com/mikeizbicki/cmc-csci040">course webpage</a>'
+    >>> compile_links('this is wrong: [course webpage]    (https://github.com/mikeizbicki/cmc-csci040)')
+    'this is wrong: [course webpage]    (https://github.com/mikeizbicki/cmc-csci040)'
+    >>> compile_links('this is wrong: [course webpage](https://github.com/mikeizbicki/cmc-csci040')
+    'this is wrong: [course webpage](https://github.com/mikeizbicki/cmc-csci040'
+    '''
+    return re.sub(r'(?<!!)\[([^\]]+)\]\(([^)]+)\)', r'<a href="\2">\1</a>', line)
+
+
+def compile_images(line):
+    '''
+    Add <img> tags.
+
+    HINT:
+    Images are formatted in markdown almost exactly the same as links,
+    except that images have a leading `!`.
+    So your code here should be based off of the <a> tag code.
+
+    >>> compile_images('[Mike Izbicki](https://avatars1.githubusercontent.com/u/1052630?v=2&s=460)')
+    '[Mike Izbicki](https://avatars1.githubusercontent.com/u/1052630?v=2&s=460)'
+    >>> compile_images('![Mike Izbicki](https://avatars1.githubusercontent.com/u/1052630?v=2&s=460)')
+    '<img src="https://avatars1.githubusercontent.com/u/1052630?v=2&s=460" alt="Mike Izbicki" />'
+    >>> compile_images('This is an image of Mike Izbicki: ![Mike Izbicki](https://avatars1.githubusercontent.com/u/1052630?v=2&s=460)')
+    'This is an image of Mike Izbicki: <img src="https://avatars1.githubusercontent.com/u/1052630?v=2&s=460" alt="Mike Izbicki" />'
+    '''
+    return re.sub(r'!\[([^\]]*)\]\(([^)]+)\)', r'<img src="\2" alt="\1" />', line)
